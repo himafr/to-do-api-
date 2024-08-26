10e2f84adbb9421d66a648d1740f69dfa1552143
hima <hemawith4@gmail.com>
2024-08-26 17:44:27 +0300

    cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser=require("body-parser")
+const cors=require("cors")
 const morgan = require('morgan');
 const todoRoute=require('./router/todoRoute')
 const userRoute=require('./router/userRoute')
@@ -8,10 +9,10 @@ const AppError=require('./utils/appError')
 const globalErrorControllers=require('./controllers/errorController');
 const authControllers=require('./controllers/authController')
 // middlewares 
+app.use(cors())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-
 
 // app.use((req,res,next)=>{
 //   req.dateTime=new Date().toLocaleTimeString();
